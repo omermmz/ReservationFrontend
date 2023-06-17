f3dbf1b218c703cb916ed5a60cb417c15aa10233
omermamaz <leomessi_23@hotmail.com>
2023-06-18 02:41:00 +0300

    radio buttona göre visibility ayarı yapıldı.

--- pages/uyeOl.js
+++ pages/uyeOl.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import resim from "../img/FT_08_06_2017_16_46_58__197.jpg";
 import UyeOlStyles from '../styles/uyeOl.module.css'
 
+
 function UyeOl() {
 
     const [valueRadio, setValueRadio] = useState('');
@@ -11,14 +12,14 @@ function UyeOl() {
         setValueRadio('True');
         setValueType("Company Employee");
         console.log(valueRadio);
-
     }
     const show2 = () => {
         setValueRadio('False');
         setValueType("Reservation User");
         console.log(valueRadio);
-
     }
+
+
     return <div className={UyeOlStyles.body}>
         <div className={UyeOlStyles.navpage}>
             <div className={UyeOlStyles.navparag}>halisaham.com</div>
@@ -35,14 +36,22 @@ function UyeOl() {
                             <h1>Kullanıcı Tipinizi Seçin:</h1>
                         </div>
                         <div className={UyeOlStyles.userChooseInputDiv}>
-                            <label className={UyeOlStyles.coUserStyle}>Company User</label>
-                            <input onClick={show} type={"radio"} className={UyeOlStyles.userNameInput} value={"HTML"}
+                            <label for={"html"} className={UyeOlStyles.coUserStyle}>Company User</label>
+                            <input onClick={show} type={"radio"} value={"HTML"}
                                    id={"html"} name={"fav-language"}/>
                         </div>
                         <div className={UyeOlStyles.userChooseInputDiv}>
-                            <label className={UyeOlStyles.coUserStyle}>Reservation User</label>
-                            <input type={"radio"} className={UyeOlStyles.userNameInput} value={"HTML"} id={"html"}
+                            <label for={"html2"} className={UyeOlStyles.coUserStyle}>Reservation User</label>
+                            <input onClick={show2} type={"radio"} value={"HTML"} id={"html2"}
                                    name={"fav-language"}/>
+                        </div>
+                        <div
+                            className={`${(valueRadio === 'True') ? UyeOlStyles.companyNameVisible : UyeOlStyles.companyNameHidden}`}>
+                            <h1 className={UyeOlStyles.companyName}>Company Name:</h1>
+                        </div>
+                        <div
+                            className={`${(valueRadio === 'True') ? UyeOlStyles.companyNameVisible : UyeOlStyles.companyNameHidden}`}>
+                            <input type={"text"} className={UyeOlStyles.userNameInput} placeholder={"Username"}/>
                         </div>
                         <div className={UyeOlStyles.userNameLabel2}>
                             <h1>Şifre:</h1>
@@ -50,6 +59,7 @@ function UyeOl() {
                         <div className={UyeOlStyles.userNameInputDiv2}>
                             <input type={"password"} className={UyeOlStyles.userNameInput} placeholder={"Password"}/>
                         </div>
+
                         <div className={UyeOlStyles.buttonStyle}>
                             <div className={UyeOlStyles.btn1}>Giriş Yap</div>
                             <p className={UyeOlStyles.buttonPara}>Henüz üye olmadıysan</p>
@@ -66,3 +76,8 @@ function UyeOl() {
 
 
 export default UyeOl
+
+
+/*<div className={`${(valueRadio==='True')? "bg-gray-100 w-64 p-2 items-center mb-3":"hidden"}`}>
+    <input type="text" value={valueCName} onChange={e => { setValueCName(e.currentTarget.value); }} name="companyName" placeholder="Company Name" className="bg-gray-100 outline-none text-sm flex-1"/>
+</div>*/
